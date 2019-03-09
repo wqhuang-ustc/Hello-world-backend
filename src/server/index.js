8c5b69252cec1b625e0d860bb1cc73036a0a1690
wqhuang-ustc <wqhuang.ustc@gmail.com>
2019-03-09 18:52:06 +0100

    8080 to 5000

--- src/server/index.js
+++ src/server/index.js
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.static('dist'));
 app.get('/api/getUptime', (req, res) => res.send({ uptime : os.uptime() }));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(process.env.PORT || 5000, () => console.log(`Listening on port ${process.env.PORT || 5000}!`));
